5fe1b079233d9fcf540e445464f3a4c81c7fa2d0
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-12-21 15:28:34 +0200

    Add exception class for validation

---
+++ Inforce.UrlShortener.BLL/Validation/MarketException.cs
@@ -0,0 +1,15 @@
+﻿namespace Inforce.UrlShortener.BLL.Validation
+{
+    public class MarketException : Exception
+    {
+        public MarketException()
+        {
+        }
+        public MarketException(string message) : base(message)
+        {
+        }
+        public MarketException(string message, Exception innerExeption) : base(message, innerExeption)
+        {
+        }
+    }
+}
