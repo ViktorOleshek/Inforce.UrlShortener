2c3304aa8c1a9e1dc9b69b13a63413396c253c6f
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-12-21 18:58:03 +0200

    Implement services

--- Inforce.UrlShortener.Abstraction/IServices/IUserService.cs
+++ Inforce.UrlShortener.Abstraction/IServices/IUserService.cs
@@ -5,7 +5,6 @@ namespace Inforce.UrlShortener.Abstraction.IServices
     public interface IUserService : ICrud<UserDto>
     {
         Task<UserDto> AuthenticateAsync(string username, string password);
-        Task RegisterUserAsync(UserDto userModel, string password);
         Task<UserDto> GetByUsernameAsync(string username);
     }
 }
---
+++ Inforce.UrlShortener.BLL/Services/UrlService.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Inforce.UrlShortener.Abstraction.DTOs;
+using Inforce.UrlShortener.Abstraction.IRepositories;
+using Inforce.UrlShortener.Abstraction.IServices;
+using Inforce.UrlShortener.BLL.Validation;
+using Inforce.UrlShortener.Entities;
+
+namespace Inforce.UrlShortener.BLL.Services
+{
+    public class UrlService : BaseService<Url, UrlDto>, IUrlService
+    {
+        private readonly IUrlRepository urlRepository;
+        public UrlService(IUnitOfWork unitOfWork, IMapper mapper)
+            : base(unitOfWork, mapper)
+        {
+            this.urlRepository = unitOfWork.UrlRepository;
+        }
+
+        protected override IRepository<Url> Repository { get => this.urlRepository; }
+
+        public Task<UrlDto> GenerateShortUrlAsync(string originalUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task AddAsync(UrlDto model)
+        {
+            var entity = await this.UnitOfWork.UrlRepository.GetByIdAsync(model.Id);
+            if (entity != null)
+            {
+                throw new MarketException("Url is already taken.");
+            }
+
+            await base.AddAsync(model);
+        }
+
+        protected override void Validate(UrlDto model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
