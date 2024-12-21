adcca4b29da9dd399328c76620db3a6cc74c8077
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-12-21 23:12:23 +0200

    Refactor code

--- Inforce.UrlShortener.BLL/Services/UrlService.cs
+++ Inforce.UrlShortener.BLL/Services/UrlService.cs
@@ -30,13 +30,43 @@ namespace Inforce.UrlShortener.BLL.Services
             {
                 throw new MarketException("Url is already taken.");
             }
-
+            //отут треба викликати GenerateShortUrlAsync і засувати в модель дані
             await base.AddAsync(model);
         }
 
         protected override void Validate(UrlDto model)
         {
-            throw new NotImplementedException();
+            // Перевірка на наявність оригінального URL
+            if (string.IsNullOrWhiteSpace(model.OriginalUrl))
+            {
+                throw new MarketException("Original URL is required.");
+            }
+
+            // Перевірка на правильний формат URL
+            //if (!Uri.IsWellFormedUriString(model.OriginalUrl, UriKind.Absolute))
+            //{
+            //    throw new MarketException("Original URL is not valid.");
+            //}
+
+            // Перевірка на наявність скороченого URL
+            if (string.IsNullOrWhiteSpace(model.ShortUrl))
+            {
+                throw new MarketException("Short URL is required.");
+            }
+
+            // Перевірка на дату створення (якщо потрібно)
+            if (model.CreatedDate > DateTime.Now)
+            {
+                throw new MarketException("Created date cannot be in the future.");
+            }
+
+            // Додаткові перевірки можуть бути додані, наприклад:
+            // Перевірка чи є CreatedBy (якщо необхідно)
+            if (string.IsNullOrWhiteSpace(model.CreatedBy))
+            {
+                throw new MarketException("Creator's name is required.");
+            }
         }
+
     }
 }
