a7b5b0162fbe677583f95de8ccb8f576d43b2baa
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-12-21 10:01:30 +0200

    Add DTOs

---
+++ Inforce.UrlShortener.Abstraction/DTOs/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Inforce.UrlShortener.Abstraction.DTOs
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string RoleName { get; set; }
+    }
+}
