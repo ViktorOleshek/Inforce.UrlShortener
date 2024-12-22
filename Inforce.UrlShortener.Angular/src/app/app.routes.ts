62971ad54771187fa90df5e6425abf1089115eff
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-12-22 22:12:31 +0200

    Update rountigs

--- Inforce.UrlShortener.Angular/src/app/app.routes.ts
+++ Inforce.UrlShortener.Angular/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { UrlTableComponent } from './components/url/url-table/url-table.component';
+import { UrlDetailComponent } from './components/url/url-detail/url-detail.component';
+import { LoginComponent } from './components/login/login.component';
+import { AuthentificatonGuard } from './shared/guard/authentificaton.guard';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+export const routes: Routes = [
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'urls', component: UrlTableComponent },
+  { path: 'urls/:id', component: UrlDetailComponent, canActivate: [AuthentificatonGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/urls', pathMatch: 'full' }, // main page
+  { path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
