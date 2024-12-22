f42fbb8dabc4dfbf9ab92548e31138dec9ed88bc
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-12-22 22:14:08 +0200

    Update configuration

--- Inforce.UrlShortener.Angular/src/app/app.config.ts
+++ Inforce.UrlShortener.Angular/src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+// import { NoopInterceptor } from './interceptors/no-sslinterceptor.interceptor';
+import { AuthentificationInterceptor } from './interceptors/authentification.interceptor';
+// import { ErrorHandlerInterceptor } from './interceptors/error-handler.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    // { provide: HTTP_INTERCEPTORS, useClass: NoopInterceptor, multi: true }, // Інтерсептор для SSL
+    { provide: HTTP_INTERCEPTORS, useClass: AuthentificationInterceptor, multi: true },  // Інтерсептор для токена JWT
+    // { provide: HTTP_INTERCEPTORS, useClass: ErrorHandlerInterceptor, multi: true }, // Інтерсептор для обробки помилок
+  ],
 };
