a117e2e52cfdf26c10dbfed3767ad7865096214a
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-12-22 22:15:06 +0200

    Fix code smell

--- Inforce.UrlShortener.Angular/src/main.ts
+++ Inforce.UrlShortener.Angular/src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {routes} from './app/app.routes';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {AuthentificationInterceptor} from './app/interceptors/authentification.interceptor';
+import {provideRouter} from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthentificationInterceptor,
+      multi: true, // Allows multiple interceptors if needed
+    },
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
